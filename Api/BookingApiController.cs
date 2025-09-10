b5eb0757523ae0ce4219ea38d8846d62842a06dc
Midhell <byronhunia@gmail.com>
2025-09-11 02:28:00 +1200

    Start of Homepage Design + Calendar
    FullCalendar,io (Calendar Library):
    ⦁	https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js
    ⦁	https://fullcalendar.io/docs/initialize-globals
    Tutorial on how to implement the FullCalendar CDN:
    ⦁	https://www.youtube.com/watch?v=hyVzloriEfo&list=PLkqwj9vc20pUyz2hLxtdUOc0cISEagtmm&ab_channel=GordonChan

---
+++ Api/BookingApiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reservation.Models;
+using System.Text.Json;
+
+
+namespace Reservation.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingApiController : ControllerBase
+    {
+    }
+}
